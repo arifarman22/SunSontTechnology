887e44bcd9e9ec2177366a18a11c8340bfaeae56
Arif Arman Pavel <pavel15-5555@diu.edu.bd>
2026-02-08 14:52:20 +0600

    Fix vite import issue for production deployment

--- server/index.ts
+++ server/index.ts
@@ -1,7 +1,7 @@
 import express, { type Request, Response, NextFunction } from "express";
 import cors from "cors";
 import { registerRoutes } from "./routes";
-import { setupVite, serveStatic, log } from "./vite";
+import { log } from "./logger";
 
 const app = express();
 app.use(cors());
@@ -53,9 +53,8 @@ app.use((req, res, next) => {
   // setting up all the other routes so the catch-all route
   // doesn't interfere with the other routes
   if (app.get("env") === "development") {
+    const { setupVite } = await import("./vite");
     await setupVite(app, server);
-  } else {
-    serveStatic(app);
   }
 
   // ALWAYS serve the app on the port specified in the environment variable PORT
